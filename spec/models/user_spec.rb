e50a74b0c56ba5b3cbda36709388a596f1604678
Williams Estrada <we4841@gmail.com>
2018-10-15 17:44:53 -0500

    Add new users fields
    fields for user's name and bio added along with its test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,28 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = User.new( email: "user@example.com", password: "foobar",
-                      password_confirmation: "foobar")
+    @user = User.new( name: "Example User", email: "user@example.com",
+                      password: "foobar", password_confirmation: "foobar",
+                      bio: "lorem ipsum dolo")
   end
 
   it "should be valid" do
     expect(@user).to be_valid
+  end
+
+  it "shold have vaild name" do
+    @user.name = " "
+    expect(@user).not_to be_valid
+  end
+
+  it "name should not be too long" do
+    @user.name = "a" * 51
+    expect(@user).not_to be_valid
+  end
+
+  it "bio should not be too long" do
+    @user.bio = "a" * 1001
+    expect(@user).not_to be_valid
   end
 
   it "should have valid email" do
